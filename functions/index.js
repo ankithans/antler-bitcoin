81398fa844d285a8bb5c4b46c139fd0426fe5c05
ankithans <ankithans1947@gmail.com>
2022-02-09 01:57:16 +0530

    feat: bitcoin cloud function to fetch price

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const axios = require("axios");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info("Hello logs!", { structuredData: true });
-	response.send("Hello from Firebase!");
-});
+const db = admin.firestore();
+
+exports.bitcoinPriceTrack = functions.pubsub
+	.schedule("every 15 minutes")
+	.onRun((context) => {
+		var config = {
+			method: "get",
+			url: "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false",
+			headers: {
+				accept: "application/json",
+			},
+		};
+
+		axios(config)
+			.then(function (response) {
+				db.doc(`bitcoin/${response.data.last_updated}`).create({
+					inr: response.data.market_data.current_price.inr,
+					usd: response.data.market_data.current_price.usd,
+				});
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	});
